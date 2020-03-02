e70bf82fed0365a18f1089a5c444a4c25a3e9dd2
root <root@terraform.example.com>
2020-03-02 23:58:35 +0700

    Fixed variable

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ provider "ovirt" {
 }
 
 
-resource "ovirt_image_transfer" "cirros_transfer" {
+resource "ovirt_image_transfer" "centos_transfer" {
   alias             = "centos7"
   source_url        = "http://cloud.centos.org/centos/7/images/CentOS-7-x86_64-GenericCloud.qcow2"
   storage_domain_id = "d1a97267-e32a-419b-851e-4c20ae67d264"
@@ -19,16 +19,16 @@ resource "ovirt_image_transfer" "cirros_transfer" {
 }
 
 resource "ovirt_vm" "tmpvm" {
-  name       = "tmpvm-for-${ovirt_image_transfer.cirros_transfer.alias}"
+  name       = "tmpvm-for-${ovirt_image_transfer.centos_transfer.alias}"
   cluster_id = "c6c4902a-5721-11ea-983e-00163e58725a"
   block_device {
-    disk_id   = ovirt_image_transfer.cirros_transfer.disk_id
+    disk_id   = ovirt_image_transfer.centos_transfer.disk_id
     interface = "virtio_scsi"
   }
 }
 
 resource "ovirt_template" "centos7" {
-  name       = "template-for-${ovirt_image_transfer.cirros_transfer.alias}"
+  name       = "template-for-${ovirt_image_transfer.centos_transfer.alias}"
   cluster_id = ovirt_vm.tmpvm.cluster_id
   // create from vm
   vm_id = ovirt_vm.tmpvm.id
